2238515eb9f6e684f7e6c85cd001b7e5465cbc8f
Nellya <63125509+ciwwwnd@users.noreply.github.com>
2021-06-21 23:59:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ bot = telebot.TeleBot(conf.TOKEN)
 
 global zodiac
 
+
 @bot.message_handler(commands=['help'])
 def send_help(message):
     bot.send_message(message.chat.id,
                      "Это бот-агрегатор гороскопов. Чтобы узнать свой гороскоп на сегодня вызовите команду /horo")
 
-@bot.message_handler(commands=['start', 'horo'])
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id,
+                     "Это бот-агрегатор гороскопов. Чтобы узнать свой гороскоп на сегодня вызовите команду /horo")
+
+# собираем клавиатуру из знаков зодиака
+@bot.message_handler(commands=['horo'])
 def send_welcome(message):
     keyboard = types.InlineKeyboardMarkup()
     aries_button = types.InlineKeyboardButton(text='овен', callback_data='aries')
@@ -36,11 +43,17 @@ def send_welcome(message):
     bot.send_message(message.chat.id,
                      "Хотите узнать свой гороскоп на сегодня? Выберите знак зодиака", reply_markup=keyboard)
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     global zodiac
 
     def get_horomail(zodiac):
+        """
+
+        получаем гороскоп с сайта гороскопы майл.ру
+
+        """
         url_template = f'https://horo.mail.ru/prediction/{zodiac}/today/'
         url = requests.get(url_template).text
         soup = BeautifulSoup(url, 'html.parser')
@@ -50,6 +63,11 @@ def callback_inline(call):
             bot.send_message(call.message.chat.id, out)
 
     def get_thousands_of_horoscopes(zodiac):
+        """
+
+        получаем гороскоп с сайта гороскопы 1001 гороскоп
+
+        """
         url_template = f'https://1001goroskop.ru/?znak={zodiac}'
         url = requests.get(url_template).text
         soup = BeautifulSoup(url, 'html.parser')
@@ -58,6 +76,11 @@ def callback_inline(call):
         bot.send_message(call.message.chat.id, horotext)
 
     def get_horo_365(zodiac):
+        """
+
+        получаем гороскоп с сайта гороскоп365
+
+        """
         url_template = f'https://goroskop365.ru/{zodiac}/'
         url = requests.get(url_template).text
         soup = BeautifulSoup(url, 'html.parser')
@@ -66,16 +89,18 @@ def callback_inline(call):
         bot.send_message(call.message.chat.id, horotext.text)
 
     def get_horo_keyboard(call):
+        """
+
+        собираем клавиатуру с тремя гороскопами
+
+        """
         keyboard = types.InlineKeyboardMarkup()
         horomail_button = types.InlineKeyboardButton(text='Гороскопы Майл.ру', callback_data='get_horomail')
-        thousands_of_horoscopes_button = types.InlineKeyboardButton(text='1001 гороскоп',
-                                                                    callback_data='get_thousands_of_horoscopes')
-        get_horo_365_button = types.InlineKeyboardButton(text='Гороскопы 365',
-                                                                    callback_data='get_horo_365')
+        thousands_of_horoscopes_button = types.InlineKeyboardButton(text='1001 гороскоп', callback_data='get_thousands_of_horoscopes')
+        get_horo_365_button = types.InlineKeyboardButton(text='Гороскопы 365', callback_data='get_horo_365')
         keyboard.add(horomail_button, thousands_of_horoscopes_button, get_horo_365_button)
         bot.send_message(call.message.chat.id, 'Ура! Пожалуйста, выберите гороскоп, который хотите получить',
                          reply_markup=keyboard)
-
 
     if call.message:
         if call.data == 'aries':
@@ -121,6 +146,6 @@ def callback_inline(call):
         if call.data == 'get_horo_365':
             get_horo_365(zodiac)
 
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
-
